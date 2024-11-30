54538be454ae04dad68684e53507eb435a88331f
Rishabh Gokhe <rishabhgokhe2004@gmail.com>
2024-11-30 23:57:42 +0530

    fixed no-unescaped-entities issue

--- app/wish/page.js
+++ app/wish/page.js
@@ -96,7 +96,7 @@ const BirthdayWishes = () => {
           onClick={handleConfettiClick}
           className="inline-flex text-black cursor-pointer items-center justify-center px-3 py-2 text-md font-medium bg-yellow-500 rounded-lg hover:bg-zinc-100 transform hover:-translate-y-1 transition ease-in-out duration-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 dark:hover:bg-white dark:hover:text-black dark:border-white shadow-lg shadow-orange-900 hover:shadow-lg hover:shadow-gray-500"
         >
-          <span className="mr-1">Let's Celebrate!</span> <BirthdayCakeIcon />
+          <span className="mr-1">Let&aposs Celebrate!</span> <BirthdayCakeIcon />
         </button>
 
         {showConfetti && (
